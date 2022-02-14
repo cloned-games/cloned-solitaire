7c515ba73fed04f8c175104d65568f953b18c49c
sinhadotabhinav <sinhabhinv@gmail.com>
2022-02-15 00:47:10 +0100

    game layout

--- src/Solitaire.js
+++ src/Solitaire.js
@@ -4,25 +4,28 @@ import './Solitaire.css';
 
 function Solitaire() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/Solitaire.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        Deal.. Deck.. Options.. Undo Score:0 Time:0
       </header>
+      <div className="game-container">
+        <div className="row">
+          <div className="row-cards">A</div>
+          <div className="row-cards">K</div>
+          <div className="row-cards">J</div>
+          <div className="row-cards">Q</div>
+          <div className="row-cards">10</div>
+        </div>
+        <div className="row">
+          <div className="row-cards">1</div>
+          <div className="row-cards">2</div>
+          <div className="row-cards">3</div>
+          <div className="row-cards">4</div>
+          <div className="row-cards">5</div>
+          <div className="row-cards">6</div>
+          <div className="row-cards">7</div>
+        </div>
+      </div>
     </div>
   );
 }
