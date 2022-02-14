639063568a477729c64b731593a8795d5219bdd5
sinhadotabhinav <sinhabhinv@gmail.com>
2022-02-14 23:56:09 +0100

    initial upload

--- src/Solitaire.js
+++ src/Solitaire.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Solitaire.css';
 
-function App() {
+function Solitaire() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/Solitaire.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -22,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default Solitaire;
--- src/Solitaire.test.js
+++ src/Solitaire.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Solitaire from './Solitaire';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Solitaire />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
